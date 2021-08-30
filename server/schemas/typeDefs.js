d790c5d72dcc8392a3fa6a4f6704e5e58e068413
qtian13 <qiushuang.tian@gmail.com>
2021-08-29 23:37:42 -0700

    add typeDef according to the api routes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,35 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type Book {
-    
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
   }
 
   type User {
-    
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    books: [Book]
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String, bookId: String, image: String, link: String, title: String): User
+    removeBook(bookId: String!): User
   }
 `;
 
